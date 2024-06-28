9ca0a4f82538be76f81deaadfb69302f041bdf94
robsonluis2222 <robsonluis2222@gmail.com>
2024-06-28 13:13:07 -0300

    feat: social medias

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './Social.css';
 
@@ -6,41 +6,52 @@ const Social = () => {
     const wpp = useRef(null);
     const face = useRef(null);
     const insta = useRef(null);
+    const [socialTab, setSocialTab] = useState(false)
 
-    const closeSocial = () => {
-        if (wpp.current) {
-            wpp.current.style.marginRight = "0px";
-            wpp.current.style.marginTop = "0px";
-        }
-        if (face.current) {
-            face.current.style.marginRight = "0px";
-        }
-        if (insta.current) {
-            insta.current.style.marginRight = "0px";
-            insta.current.style.marginTop = "0px";
-        }
-    }
     
     const openSocial = () => {
-        if (wpp.current) {
-            wpp.current.style.marginRight = "20px";
-            wpp.current.style.marginTop = "-50px";
+        if(socialTab === false){
+            if (wpp.current) {
+                wpp.current.style.marginRight = "20px";
+                wpp.current.style.marginTop = "-50px";
+            }
+            if (face.current) {
+                face.current.style.marginRight = "60px";
+            }
+            if (insta.current) {
+                insta.current.style.marginRight = "20px";
+                insta.current.style.marginTop = "50px";
+            }
+            setSocialTab(true)
+        } else {
+            if (wpp.current) {
+                wpp.current.style.marginRight = "0px";
+                wpp.current.style.marginTop = "0px";
+            }
+            if (face.current) {
+                face.current.style.marginRight = "0px";
+            }
+            if (insta.current) {
+                insta.current.style.marginRight = "0px";
+                insta.current.style.marginTop = "0px";
+            }
+            setSocialTab(false)
         }
-        if (face.current) {
-            face.current.style.marginRight = "60px";
-        }
-        if (insta.current) {
-            insta.current.style.marginRight = "20px";
-            insta.current.style.marginTop = "50px";
-        }
+        
     };
 
     return (
         <div className='book' onClick={openSocial}>
-            <div className='wpp' ref={wpp}></div>
-            <div className='face' ref={face}></div>
-            <div className='insta' ref={insta}></div>
-            <i className="bi bi-arrow-return-right" onClick={closeSocial}></i>
+            <div className='wpp' ref={wpp}>
+                <img src="https://i.imgur.com/hm8h6Hp.png" alt="" />
+            </div>
+            <div className='face' ref={face}>
+                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/facebook-app-round-white-icon.png" alt="" />
+            </div>
+            <div className='insta' ref={insta}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png" alt="" />
+            </div>
+            <i className="bi bi-arrow-return-right"></i>
         </div>
     );
 };
